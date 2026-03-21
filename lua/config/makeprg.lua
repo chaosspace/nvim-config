c032c2eea805d70162d7dcb3e3637dd83c90a1f8
Chaos <940010004@qq.com>
2026-03-22 03:03:05 +0800

    feat: makeprg.

--- lua/config/makeprg.lua
+++ lua/config/makeprg.lua
@@ -1,24 +1,27 @@
 -- Auto set makeprg based on filetype
-vim.api.nvim_create_autocmd("FileType", {
+-- Use BufEnter with defer_fn to override LSP defaults (rust_analyzer sets rustc)
+vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
   group = vim.api.nvim_create_augroup("makeprg", { clear = true }),
   callback = function(args)
-    local ft = args.match
-    local makeprg_map = {
-      c = "gcc -Wall -g % -o %<",
-      cpp = "g++ -Wall -g % -o %<",
-      java = "javac %",
-      python = "python %",
-      go = "go build -o %< .",
-      rust = "cargo build",
-      javascript = "node %",
-      typescript = "tsc %",
-      ruby = "ruby %",
-      lua = "luac -p %",
-      sh = "bash %",
-      zsh = "zsh %",
-    }
-    if makeprg_map[ft] then
-      vim.opt.makeprg = makeprg_map[ft]
-    end
+    vim.defer_fn(function()
+      local ft = vim.bo.filetype
+      local makeprg_map = {
+        c = "gcc -Wall -g % -o %<",
+        cpp = "g++ -Wall -g % -o %<",
+        java = "javac %",
+        python = "python %",
+        go = "go build -o %< .",
+        rust = "cargo build",
+        javascript = "node %",
+        typescript = "tsc %",
+        ruby = "ruby %",
+        lua = "luac -p %",
+        sh = "bash %",
+        zsh = "zsh %",
+      }
+      if makeprg_map[ft] then
+        vim.opt.makeprg = makeprg_map[ft]
+      end
+    end, 50)
   end,
 })
